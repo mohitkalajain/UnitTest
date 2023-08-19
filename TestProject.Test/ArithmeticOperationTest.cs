b3db70e7dafd8e31e3cb17276517da4256fb5738
Mohit Kala <73868188+mohitkalajain@users.noreply.github.com>
2023-08-19 19:53:50 +0530

    init

---
+++ TestProject.Test/ArithmeticOperationTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestProject.Test
+{
+    [TestFixture]
+    public class ArithmeticOperationTest
+    {
+        [Test]
+        public void AdditionOfTwoNumber_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            ArithmeticOperation calc = new();
+
+            //Act
+            int result = calc.AdditionOfTwoNumber(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void AdditionOfTwoNumber_InputTwoInt_GetWrongAddition()
+        {
+            //Arrange
+            ArithmeticOperation calc = new();
+
+            //Act
+            int result = calc.AdditionOfTwoNumber(10, 20);
+
+            //Assert
+            Assert.AreEqual(40, result);
+        }
+
+
+
+    }
+}
---
+++ TestProject/ArithmeticOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class ArithmeticOperation
+    {
+        public int AdditionOfTwoNumber(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int MultiplicationOfTwoNumber(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+    }
+}
